c2a0d6be8a279773eb976919ef3714a5ec19e06d
Paul Braetz <paul.braetz@rhomicro.com>
2023-11-13 19:07:04 +0100

    recorded looping vid

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,16 +1,21 @@
 #pragma warning disable
 
-Enumerable.Range(0, Int32.MaxValue)
-    .Select(i => (Value: i, Width: 1000))
-    .Select(t => (t.Width, Range: Enumerable.Range(1, t.Width / 2).Concat(Enumerable.Range(1, (t.Width / 2) - 2).Select(v => (t.Width / 2) - v)).ToArray(), t.Value))
-    .Select(t => t.Range[t.Value % (t.Width - 2)])
-    .Select(static v => v % 15 == 0 ? "FizzBuzz" : v % 3 == 0 ? "Fizz" : v % 5 == 0 ? "Buzz" : v.ToString())
-    .Select(static o => (Output: o, Amplitude: 20, Period: 0.1d))
-    .Select(static (t, i) => (t.Amplitude, t.Output, Sine: Console.WindowWidth / 2 + (Int32)(Math.Sin(t.Period * i) * t.Amplitude)))
-    .Select(static (t, i) => (t.Output, t.Sine, Color: (ConsoleColor)(i / 3 % 15 + 1)))
-    .Select(static t => { Console.ForegroundColor = t.Color; return t; })
-    .Select(static t => { Console.CursorLeft = t.Sine; return t; })
-    .Select(static t => (t.Sine, Delay: 75, t.Output))
-    .Select(static t => { Thread.Sleep(t.Delay); return t.Output; })
+Enumerable.Range(1, Int16.MaxValue)
+    .Select(static i => (Value: i, ValueRangeWidth: 200, Divisor: 202.5 * 2 * Math.PI, Amplitude: 15, Delay: 50))
+    .Select(static t => { Thread.Sleep(t.Delay); return t; })
+    .Select(static t => (t.Value, Inflection: (Int32)Math.Ceiling(t.ValueRangeWidth / 2d), ClampedValue: t.Value % t.ValueRangeWidth, t.Amplitude, Period: t.ValueRangeWidth / t.Divisor, t.ValueRangeWidth))
+    .Select(static t => (
+        PeriodicValue: t.ClampedValue > t.Inflection ? t.ValueRangeWidth - t.ClampedValue : t.ClampedValue,
+        t.Value,
+        t.Amplitude,
+        t.Period))
+    .Select(static t => (
+        Output: t.PeriodicValue % 15 == 0 ? "FizzBuzz" :
+                t.PeriodicValue % 3 == 0 ? "Fizz" :
+                t.PeriodicValue % 5 == 0 ? "Buzz" :
+                t.PeriodicValue.ToString(),
+        CursorLeft: Console.WindowWidth / 2 + (Int32)(Math.Sin(t.Value * t.Period) * t.Amplitude),
+        Color: (ConsoleColor)(t.PeriodicValue % 15 + 1)))
+    .Select(static t => { Console.CursorLeft = t.CursorLeft; Console.ForegroundColor = t.Color; return t.Output; })
     .Select(static o => { Console.WriteLine(o); return 0; })
-    .ToArray();
+    .ToList();
